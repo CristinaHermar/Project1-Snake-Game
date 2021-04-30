08772e44e8cf2affd38135ee7fd4a1cbeff567e1
herguejuan <josejuanherreraguerra@gmail.com>
2021-04-30 18:21:01 -0500

    Actualizando repo con nueva versión de Snake

--- Snake.py
+++ Snake.py
@@ -3,36 +3,25 @@ import time
 import random
 import os
 
-
-# resources :
-#bgpic = 'C:/Users/Iron/Pictures/fondo-snake2.jpg'
-
-
-#win = turtle.Screen()
-#win.addshape(bgpic)
-
-#sh = turtle.Turtle()
-#sh.shape(bgpic)
-
-# Shuting the window down :
-#turtle.mainloop()
-
-# Area de juego
-
 ventana = turtle.Screen()
-ventana.title(' Juguemos Snake ')
-ventana.bgcolor('white')
+ventana.bgpic()
+ventana.bgpic("landscape3.gif")
+ventana.title(' Snake ')
 ventana.setup(width = 700, height = 700)
 ventana.tracer(0)								# Hace animaciones mas interactivas con el usuario
-#ventana.addshape(bgpic)
+
 # Head
 
 head = turtle.Turtle()
 head.speed(0)
 head.shape('square')
+head.color('black')
 head.penup()									# Con este método no deja rastro nuestro elemento
 head.goto(0,0)
 head.direction = 'stop'
+
+# Body
+body = []
 
 # Food
 
@@ -41,11 +30,8 @@ food.speed(0)
 food.shape('circle')
 food.penup()
 food.goto(100,0)
-food.color('red')
+food.color('#FFD700')
 food.direction = 'left'
-
-# Body
-body = []
 
 #Marcador
 
@@ -54,7 +40,7 @@ high_score = 0
 
 marcador = turtle.Turtle()
 marcador.speed(0)
-marcador.color('Black')
+marcador.color('White')
 marcador.penup()
 marcador.hideturtle()
 marcador.goto(0,310)
@@ -77,7 +63,7 @@ def izquierda():
 def exit():
 	ventana.bye()
 
-def mov():
+def movimiento():
 	if head.direction == 'up':
 		y = head.ycor()
 		head.sety (y + 20)
@@ -94,31 +80,30 @@ def mov():
 		x = head.xcor()
 		head.setx (x - 20)
 
-def quitartiempo(x,y):
-	if x == 0.05:
-		y = y + 0.05
-		return y
 
 # Haciendo que las flechas dirijan 
+
 ventana.listen()							# Esta funcion '.listen' permite que la ventana de trabajo capte las entradas del teclado, como un input'
 ventana.onkeypress(arriba, 'Up')
 ventana.onkeypress(abajo, 'Down')
 ventana.onkeypress(derecha, 'Right')
 ventana.onkeypress(izquierda, 'Left')
 ventana.onkeypress(exit,'x')
-## Definir niveles
 
-tiempo = 0.15
+## Definir tiempo
+
+tiempo = 0.20
 nuevo_tiempo = tiempo
 
 while True:
+
 	ventana.update()
 
-	# Colisiones bordes
+		# Colisiones bordes
 
 	if head.xcor() > 340 or head.xcor() < -340 or head.ycor() > 340 or head.ycor() < -340:
 		time.sleep(1)
-		tiempo = 0.15
+		tiempo = 0.20
 		nuevo_tiempo = tiempo	
 		head.goto(0,0)
 		head.direction = 'stop'
@@ -134,32 +119,30 @@ while True:
 		marcador.clear()
 		marcador.write("Score:  {}    Hight Score: {} ".format(score, high_score) , align="center", font = ("Lemon", 20, "normal"))
 
-
 		
 	if head.distance(food) < 20:
 		x = random.randint(-330, 330)
 		y = random.randint(-330, 300)
 		food.goto(x,y)
 
-
 		new_body = turtle.Turtle()
 		new_body.speed(0)
 		new_body.shape('square')
-		new_body.color('black')									
+		new_body.color('#ADFF2F')									
 		new_body.penup()
 		body.append(new_body)
 
-
 		score += 1
+		if score > high_score:
+			high_score = score
+
+		#Agregando dificultad
+
 		tiempo -= 0.01
 		nuevo_tiempo = tiempo
 		
-		if score > high_score:
-			high_score = score
-		
-		if len(body) > 12:
+		if len(body) > 18:
 			nuevo_tiempo = 0.025
-
 
 		marcador.clear()
 		marcador.write("Score:  {}    Hight Score: {} ".format(score, high_score) , align="center", font = ("Lemon", 20, "normal"))
@@ -176,10 +159,12 @@ while True:
 		x = head.xcor()
 		y = head.ycor()
 		body[0].goto(x,y)
-		# Haciendo que las flechas dirijan 
 
-	mov()
 
+# Haciendo que las flechas dirijan 
+	
+	movimiento()
+	
 	# Colisiones con el cuerpo
 
 	for i in body:
@@ -188,7 +173,7 @@ while True:
 			head.goto(0,0)
 			head.direction = 'stop'
 
-			tiempo = 0.15
+			tiempo = 0.20
 			nuevo_tiempo = tiempo	
 
 			for i in body:
